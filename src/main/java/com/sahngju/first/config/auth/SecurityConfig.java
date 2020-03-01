3456191448fe11e5ad6d2979cc470820470677dd
sahngju <sahngju@gmail.com>
2020-03-01 18:40:39 +0900

    chapter2

---
+++ src/main/java/com/sahngju/first/config/auth/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.sahngju.first.config.auth;
+
+import com.sahngju.first.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+      http.csrf().disable().headers().frameOptions().disable()
+              .and()
+                .authorizeRequests()
+                .antMatchers("/","/css/**","/images/**","/js/**","/h2-console/**").permitAll()
+                .antMatchers("/api/vi/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated()
+              .and()
+                .logout()
+                  .logoutSuccessUrl("/")
+              .and()
+                .oauth2Login()
+                  .userInfoEndpoint()
+                    .userService(customOAuth2UserService);
+    }
+}
--- src/main/java/com/sahngju/first/web/IndexController.java
+++ src/main/java/com/sahngju/first/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.sahngju.first.web;
 
+import com.sahngju.first.config.auth.LoginUser;
+import com.sahngju.first.config.auth.dto.SessionUser;
 import com.sahngju.first.service.PostsService;
 import com.sahngju.first.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -10,15 +12,22 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.mail.Session;
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
+        if(user != null) {
+            model.addAttribute("userName",user.getName());
+        }
         return "index";
     }
     @GetMapping("/posts/save")
